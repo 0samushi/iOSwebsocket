20b4497d0d07d681bdf1e475ae1cf144f8d5e522
Osamushi <mad.naname@gmail.com>
2015-04-17 20:10:13 +0900

    Add ios connection.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var io = require('socket.io').listen(app);
 app.listen(3000);
 
 function handler(req, res) {
+    console.log('リクエスト！');
     fs.readFile(__dirname + '/index.html', function(err, data) {
         if (err) {
             res.writeHead(500);
@@ -22,5 +23,9 @@ io.sockets.on('connection', function(socket) {
     socket.on('emit_from_client', function(data) {
         console.log(data);
         io.sockets.emit('emit_from_server', '[' + data.name + ']' + data.msg);
+    });
+    socket.on('emit_from_ios', function(data) {
+        console.log(data);
+        socket.broadcast.emmit('emit_from_server', '[ios]' + data.msg);
     });
 });
