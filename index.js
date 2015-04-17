881f766e59884dea065337b45d1629c9981d7678
Osamushi <mad.naname@gmail.com>
2015-04-17 19:08:00 +0900

    Implement socket.io

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-/**
- * Created by osamu on 4/17/15.
- */
+var fs = require('fs');
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
 
-var http = require('http');
-var server = http.createServer();
-server.on('request', function (req, res) {
-    res.writeHead(200, {'Cotent-Type': 'plain/text'});
-    res.write('はろろ');
-    res.end();
-});
-server.listen(3000);
+app.listen(3000);
+
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html', function(err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('エラー')
+        }
+        res.writeHead(200);
+        res.write(data);
+        res.end();
+    });
+};
 
 console.log('サーバー起動中...');
+
+io.sockets.on('connection', function(socket) {
+    socket.on('emit_from_client', function(data) {
+        console.log(data);
+        io.sockets.emit('emit_from_server', '[' + data.name + ']' + data.msg);
+    });
+});
