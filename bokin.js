b421bad6054d5f0588d65c0d6e7769547acf9d92
Osamushi <mad.naname@gmail.com>
2015-04-18 16:20:48 +0900

    Implement socket.io init.

---
+++ bokin.js
@@ -0,0 +1,31 @@
+/**
+ * Created by osamu on 4/18/15.
+ */
+
+
+var fs = require('fs');
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+
+app.listen(3000);
+
+function handler(req, res) {
+    fs.readFile(__dirname + '/bokin.html', function(err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('エラー');
+        }
+        res.writeHead(200);
+        res.write(data);
+        res.end();
+    });
+}
+
+console.log('サーバー起動中');
+
+io.sockets.on('connection', function(socket) {
+    socket.on('emit_from_web', function(data) {
+        console.log(data);
+        io.sockets.emit('emit_from_server', data.price + '円');
+    });
+});
